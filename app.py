ab1432acab69ab3779b3d8a543dac385b5fb2a0b
H KJ <61111psp@gmail.com>
2020-04-18 21:16:54 +0800

    make it better

--- app.py
+++ app.py
@@ -35,10 +35,10 @@ def callback():
 
     return 'OK'
 
-
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text="abc")
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 
