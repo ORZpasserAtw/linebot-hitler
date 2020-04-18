43c7856d4d769fbd0011d9a75ff3fcedf76080ec
H KJ <61111psp@gmail.com>
2020-04-18 21:14:41 +0800

    make it better

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('tWtoT0Ov770ISQVJqvjfvB3sElVfLVc+QkFvJ4Ug41CfzhQefISru3q1BAIgN67kO+hfd1kEkYFFVfZiXAekEvSOdgtcGbYOWKIQ5lST9x1QYZYmBPr4JDwUkUXQmydnviHU1FthYD7mcKv1JMzMJwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(
+    'tWtoT0Ov770ISQVJqvjfvB3sElVfLVc+QkFvJ4Ug41CfzhQefISru3q1BAIgN67kO+hfd1kEkYFFVfZiXAekEvSOdgtcGbYOWKIQ5lST9x1QYZYmBPr4JDwUkUXQmydnviHU1FthYD7mcKv1JMzMJwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('747d5974fd7bbae6b9534cffc56e088d')
 
 
@@ -35,11 +36,10 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message="abc")
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="fuck you bitch"))
+    message = TextSendMessage(text="abc")
+    line_bot_api.reply_message(event.reply_token, message)
 
 
 if __name__ == "__main__":
