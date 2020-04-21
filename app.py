d98d44ff53e18d086ed555fa16774f921cab42eb
H KJ <61111psp@gmail.com>
2020-04-22 00:27:23 +0800

    Flex URI

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def handle_message(event):
                 alt_text="Flex Message 導航",
                 contents=BubbleContainer(
                     body=BoxComponent(layout="vertical", contents=[TextComponent(text="TEXT")]),
-                    footer=BoxComponent(layout="horizontal",contents=[ButtonComponent(action=URIAction(label="開啟 Google 地圖",uri("https://www.google.com.tw/maps/place/"+locations[0][0])))])
+                    footer=BoxComponent(layout="horizontal",contents=[ButtonComponent(action=URIAction(label="開啟 Google 地圖",uri=("https://www.google.com.tw/maps/place/"+str(locations[0][0]))))])
                     )
             )
         line_bot_api.reply_message(event.reply_token, flex_message)
