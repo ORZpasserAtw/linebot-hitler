69985ca0cf3b2d85dda706a9af00ce4509599f59
H KJ <61111psp@gmail.com>
2020-04-21 23:29:21 +0800

    Flex LW

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, StickerSendMessage, TemplateSendMessage, FlexSendMessage, URIAction, MessageAction, MessageTemplateAction, RichMenu, RichMenuSize, RichMenuArea, RichMenuBounds, CarouselTemplate, CarouselColumn, ConfirmTemplate, BubbleContainer, BoxComponent, TextComponent, ButtonComponent,FlexContainer, BubbleStyle, BlockStyle
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, StickerSendMessage, TemplateSendMessage, FlexSendMessage, URIAction, MessageAction, MessageTemplateAction, RichMenu, RichMenuSize, RichMenuArea, RichMenuBounds, CarouselTemplate, CarouselColumn, ConfirmTemplate, BubbleContainer, BoxComponent, TextComponent, ButtonComponent, FlexContainer, BubbleStyle, BlockStyle
 )
 
 app = Flask(__name__)
@@ -80,7 +80,8 @@ def handle_message(event):
         if locations[0][0] in event.message.text:
             flex_message = FlexSendMessage(
                 alt_text="Flex Message 導航",
-                contents=BubbleContainer(direction="ltr",body=BoxComponent(layout="vertical",contents=[TextComponent(text="TEXT")]))
+                contents=BubbleContainer(direction="ltr", body=BoxComponent(
+                    contents=[TextComponent(text="TEXT")]))
             )
         line_bot_api.reply_message(event.reply_token, flex_message)
     elif event.message.text == "天氣及空氣品質":
