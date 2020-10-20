e338db8f28143c14f7bdb1238732268f4f3cc6a3
H KJ <61111psp@gmail.com>
2020-10-20 17:22:21 +0800

    Help

--- app.py
+++ app.py
@@ -417,16 +417,13 @@ def handle_message(event):
     elif event.message.text == "幫助":
         flex_message = FlexSendMessage(
             alt_text="幫助 Flex",
-            contents=BubbleContainer(size="giga",body=BoxComponent(layout="vertical",contents=[
-                    BoxComponent(layout="horizontal", contents=[
+            contents=BubbleContainer(size="giga",body=BoxComponent(layout="horizontal",contents=[
+                    BoxComponent(layout="vertical", contents=[
                         TextComponent(text="呼叫助理",action=MessageAction(text="呼叫助理-幫助"),decoration="underline",align="center",size="sm",margin="xxl"),
                         TextComponent(text="推薦行程",action=MessageAction(text="推薦行程-幫助"),decoration="underline",align="center",size="sm",margin="xxl"),
                         TextComponent(text="記帳小本本",action=MessageAction(text="記帳小本本-幫助"),decoration="underline",align="center",size="sm",margin="xxl")
                     ]),
-                    BoxComponent(layout="horizontal", contents=[
-                        TextComponent(text="　",size="xxs")
-                    ]),
-                    BoxComponent(layout="horizontal", contents=[
+                    BoxComponent(layout="vertical", contents=[
                         TextComponent(text="天氣及空氣品質",action=MessageAction(text="天氣及空氣品質-幫助"),decoration="underline",align="center",size="sm",margin="xxl"),
                         TextComponent(text="油價",action=MessageAction(text="油價-幫助"),decoration="underline",align="center",size="sm",margin="xxl"),
                         TextComponent(text="　",size="sm")
