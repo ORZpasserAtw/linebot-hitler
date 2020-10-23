2cc04218569d079335609eb2889b2502cef45c33
H KJ <61111psp@gmail.com>
2020-10-23 23:32:51 +0800

    style

--- app.py
+++ app.py
@@ -418,12 +418,12 @@ def handle_message(event):
         flex_message = FlexSendMessage(
             alt_text="幫助 Flex",
             contents=CarouselContainer(contents=[
-                    BubbleContainer(size="kilo",body=BoxComponent(layout="vertical",spacing="lg",contents=[
+                    BubbleContainer(size="kilo",body=BoxComponent(layout="vertical",spacing="sm",contents=[
                         ButtonComponent(action=MessageAction(label="呼叫助理", text="呼叫助理-幫助"),style="primary"),
                         ButtonComponent(action=MessageAction(label="推薦行程", text="推薦行程-幫助"),style="primary"),
                         ButtonComponent(action=MessageAction(label="記帳小本本", text="記帳小本本-幫助"),style="primary")
                     ])),
-                    BubbleContainer(size="kilo",body=BoxComponent(layout="vertical",spacing="lg",contents=[
+                    BubbleContainer(size="kilo",body=BoxComponent(layout="vertical",spacing="sm",contents=[
                         ButtonComponent(action=MessageAction(label="天氣及空氣品質", text="天氣及空氣品質-幫助"),style="primary"),
                         ButtonComponent(action=MessageAction(label="油價", text="油價-幫助"),style="primary"),
                         TextComponent(text="　")
