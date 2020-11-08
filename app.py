6a2ba990822a2cfd680fbb97fee4521385dfd9b9
H KJ <61111psp@gmail.com>
2020-11-08 19:52:06 +0800

    GAS FIX

--- app.py
+++ app.py
@@ -313,7 +313,10 @@ def handle_message(event):
         soup = BeautifulSoup(response.text, "html.parser")
         def printt(response,soup):
             timer = soup.find("ul",{"id": "gas-price"}).find("li").find("p")
-            return timer.get_text().replace(" ", "")[:-9]
+        if "明日" in timer.get_text():
+            return timer.get_text().replace(" ", "")[8:-5]
+        else:
+        return timer.get_text().replace(" ", "")[:-10] 
         def printgas(response,soup):
             if soup.find("h2",{"class": "down"}) != None:
                 pregas = "汽油每公升降"
