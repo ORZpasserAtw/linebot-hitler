628ecc2dcf621d72688382b6d7813733c74a0d7b
H KJ <61111psp@gmail.com>
2020-12-19 17:28:22 +0800

    fff

--- app.py
+++ app.py
@@ -398,7 +398,7 @@ def handle_message(event):
                 ])
             )
         line_bot_api.reply_message(event.reply_token, [
-                TextSendMessage(text="歡迎加入Linebot\n讓你輕鬆管理生活大小事\n以下是此行動助理的功能說明"),
+                TextSendMessage(text="歡迎加入行動助理\n讓你輕鬆管理生活大小事\n以下是此助理的功能說明"),
                 flex_message
                 ])
     elif event.message.text == "小遊戲":
